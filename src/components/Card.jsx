c9a370d10a24b1b31b8f3e23f1180971c8bb3a5e
Andre Barros <andrebarros7201@gmail.com>
2024-06-12 16:21:15 +0100

    improved button style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
+import "./styles/cards.css";
+
 function Card({ flag }) {
   return (
-    <div className="card">
-      <button>
-        <img src={`https://flagsapi.com/${flag}/flat/64.png`} alt={flag} />
-        <p>{flag}</p>
-      </button>
-    </div>
+    <button className="card">
+      <img src={`https://flagsapi.com/${flag}/flat/64.png`} alt={flag} />
+      <p>{flag}</p>
+    </button>
   );
 }
 
